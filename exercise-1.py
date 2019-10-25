fc4a0583d8ca901246ee33c55d5d379e9b243893
pcostam <pcosta.margarida@gmail.com>
2019-10-25 16:45:17 +0100

    refactorization

--- exercise-1.py
+++ exercise-1.py
@@ -11,32 +11,32 @@ import re
 stop_words = set(nltk.corpus.stopwords.words("english"))
 
 def main():
-    train, test = get_20_news_group(30)
-    keys = tf_idf(train, test)
+    docs,test = get_20_news_group(30)
+    keys = tf_idf(docs,test)
     print(keys)
 
 #loads train set and test doc
-#@input:size disered for the train set
-#@return: set of train set and one test doc 
-def get_20_news_group(size_train):
-    train = fetch_20newsgroups(subset = 'train', shuffle = True) #The F-score will be lower because it is more realistic.
-    test  = fetch_20newsgroups(subset = 'test') #The F-score will be lower because it is more realistic.
-    
-    return train.data[:30], [test.data[0]]
+#@input:size disered for the docs set
+#@return: set of docs set and one test doc 
+def get_20_news_group(size):
+    docs = fetch_20newsgroups(subset = 'all') 
+    doc_test  = fetch_20newsgroups(subset = 'test', shuffle=False)
+    return docs.data[:size + 1], [doc_test.data[0]]
 
 def preprocess(text):
     text = text.lower()
-    #TODO: esta coisa esta a dar passar hifens sem ser entre palavras...
+  
     #Remove puntuation, except hifen
     remove = string.punctuation
     remove = remove.replace("-", "") # don't remove hyphens
     pattern = r"[{}]".format(remove) # create the pattern
     text = re.sub(pattern, "", text)
-            
+    text = re.sub("r'^(?!.*?(\w)+(\w+[-]\w+)*([-]\w+)*).*", "", text)
     text = re.sub(r'[^\D]'  ,'',text)
     text = re.sub(r'[\n]',' ', text)
     
     return text
+
 def generate_clauses(docs):
     clauses = list()
     for doc in docs:
@@ -55,102 +55,46 @@ def generate_ngrams(clauses, min_ngrams, max_ngrams):
             for ngram in ngrams:
                 group = " ".join(ngram)
                 vocab.append(group)
-    print(">>>vocab len", vocab)
-    
-    return set(vocab)
+  
+    return list(set(vocab))
 
-    
 #Tokenizes set train and test doc by sentence
 #calls: sentence_preprocess, tf_idf_train, tf_idf_test and calc_prediction
 #@input:train set and doc test
 #@return: set of keyphrases 
-def tf_idf(train, test):
-    for i in range(0,len(train)):
-        train[i] = preprocess(train[i])
+def tf_idf(docs, test):
+    for i in range(0,len(docs)):
+        docs[i] = preprocess(docs[i])
   
     #TRAIN
-    vectorizer_tfidf = tf_idf_train(train)   
+    vectorizer_tfidf = tf_idf_train(docs)   
     
     #TEST
-    phrases = nltk.sent_tokenize(test[0])
-    candidates_tokanize_test = sentence_preprocess(phrases)
-    test_vector = tf_idf_test(vectorizer_tfidf, candidates_tokanize_test)
+    test_vector = tf_idf_test(vectorizer_tfidf, test)
     
     keys = calc_prediction(test_vector, vectorizer_tfidf)
     
     return keys
-    
-#cleaning each sentence
-#@input:phrase
-#@return: phrases in lowercase with no pontuation and no digits 
-def sentence_preprocess(phrases):
-    candidates =[]
-    for phrase in phrases:  
-        phrase = phrase.lower()
-        #Remove puntuation, except hifen
-        remove = string.punctuation
-        remove = remove.replace("-", "") # don't remove hyphens
-        pattern = r"[{}]".format(remove) # create the pattern
-        phrase = re.sub(pattern, "", phrase)
-        
-        phrase = re.sub(r'[^\D]'  ,'',phrase)
-        phrase = re.sub(r'[\n]',' ', phrase)
-        candidates.append(phrase)
-        
-    return candidates
 
 
-#Creates vectorizer and fits it to the train set
+#Creates vectorizer and fits it to the docs
 #@input:train set already pre-processed 
 #@return: vectorizer 
-def tf_idf_train(train, vocab=[]):
-    if(vocab == []):
-        unigrams = list()
-        bigrams = list()
-        clauses = list()
-        words = list()
+def tf_idf_train(docs):
+        clauses = generate_clauses(docs)
+        vocabulary = generate_ngrams(clauses, 2, 3)
         
-        #Learn the vocabulary dictionary and return term-document matrix
-        for doc in train:
-            #to generate bigrams/trigrams
-            phrases = nltk.sent_tokenize(doc)
-            clauses = clauses + phrases
-            
-        for clause in clauses:
-            clauses_tokenized = nltk.word_tokenize(clause)
-            bigrams = list(nltk.ngrams(clauses_tokenized,2))
-            trigrams = list(nltk.ngrams(clauses_tokenized,3))
-            for bigram in bigrams:
-                group = " ".join(bigram)
-                words.append(group)
-            for trigram in trigrams:
-                group = " ".join(bigram)
-                words.append(group)
-                
-      
-        #to generate unigrams
-        unigrams = unigrams + nltk.word_tokenize(doc)
-    
-        vocabulary = list(set(words)) + list(set(unigrams))
-        
-        
-       
         vectorizer_tfidf = TfidfVectorizer(vocabulary=vocabulary, use_idf = True, analyzer = 'word', ngram_range=(1,3), stop_words = 'english')
-        vectorizer_tfidf.fit_transform(train)
+        vectorizer_tfidf.fit_transform(docs)
             
         return vectorizer_tfidf
-    else:
-        vectorizer_tfidf = TfidfVectorizer(vocabulary=vocab, use_idf = True, analyzer = 'word', ngram_range=(1,3), stop_words = 'english')
-        vectorizer_tfidf.fit_transform(train)
-            
-        return vectorizer_tfidf
-        
+  
 
 #applies the learned vocabulary to the test doc
 #@input:vectorizer and test tokanized
 #@return: vectorizer 
-def tf_idf_test(vectorizer_tfidf, candidates_tokanize_test):
-    return vectorizer_tfidf.transform(candidates_tokanize_test)
+def tf_idf_test(vectorizer_tfidf, doc_test):
+    return vectorizer_tfidf.transform(doc_test)
 
 #computes tf-idf final scores according to it len 
 #@input: matrix with tf-idf scores, feature_names, char_or_word
@@ -159,11 +103,9 @@ def tf_idf_scores(test_vector, feature_names,chars_or_words="words"):
 
     test_vector = test_vector.toarray()
    
-    #count = 0
     for i in range(0, test_vector.shape[0]):
         for j in range(0, test_vector.shape[1]):
             if test_vector[i,j] != 0:
-                #count += 1
                 if chars_or_words == 'chars':
                     test_vector[i,j] = test_vector[i,j] * len(feature_names[j])   
                  
@@ -171,7 +113,6 @@ def tf_idf_scores(test_vector, feature_names,chars_or_words="words"):
                 elif chars_or_words == 'words':
                     test_vector[i,j] =  test_vector[i,j] * len(feature_names[j].split())
     
-    #print("count", count)
     test_vector = sparse.csr_matrix(test_vector)
     return test_vector
 
